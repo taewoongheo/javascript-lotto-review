8a319ad1e5dca1aae9870413b4c4a03ac96b7447
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2025-01-22 15:00:05 +0900

    chore(app): 코드 포맷을 위한 eslint, prettier 설정

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  pluginJs.configs.recommended,
+  ...compat.extends('airbnb-base'),
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 'latest',
+    },
+    rules: {
+      'import/extensions': ['error', 'ignorePackages'],
+      'class-methods-use-this': 'off',
+    },
+  },
+  {
+    files: ['eslint.config.js'],
+    rules: {
+      'no-underscore-dangle': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
+];
